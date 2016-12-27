7a1ff3b705892a38f0687c44c0ec5bc7cb05e4eb
Ron Kurr <kurr@kurron.org>
2016-12-27 10:50:03 -0500

    Upgrade to current Packer

--- scripts/install-virtualbox.sh
+++ scripts/install-virtualbox.sh
@@ -3,7 +3,7 @@
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
 mount -o loop $VBOX_ISO /mnt
-/mnt/VBoxLinuxAdditions.run --nox11
+/mnt/VBoxLinuxAdditions.run
 umount /mnt
 
 #Remove the guest additions ISO so it doesn't take up space in the finished base box:
