faf52cb2fe91d1c8ec5a02f190456c64301aa61d
Ron Kurr <kurr@kurron.org>
2017-04-30 19:55:30 -0400

    Upgrade to VirtualBox 5.1.22

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 export PACKER_LOG=activate
 #export PACKER_LOG_PATH=/tmp
 
-packer validate packer.json
-packer build --force packer.json
+PACKER_FILE=${1:-packer.json}
+
+packer validate ${PACKER_FILE}
+packer build --force ${PACKER_FILE}
 vagrant box add --clean --force --name xenial-server-docker vagrant/xenial-server-docker.box
 vagrant box list
