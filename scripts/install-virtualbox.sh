ee6bbc456fe2a947ae9d2e03f6c9346c2b6f2926
Ron Kurr <kurr@kurron.org>
2017-04-30 18:43:28 -0400

    Upgrade to VirtualBox 5.1.22

--- scripts/install-virtualbox.sh
+++ scripts/install-virtualbox.sh
@@ -2,9 +2,14 @@
 
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
-mount -o loop $VBOX_ISO /mnt
+mount -o loop ${VBOX_ISO} /mnt
 /mnt/VBoxLinuxAdditions.run
 umount /mnt
 
 #Remove the guest additions ISO so it doesn't take up space in the finished base box:
-rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
+rm -rf /home/vagrant/${VBOX_ISO}
+
+# mount as a CD-ROM device
+#mount /dev/cdrom /mnt
+#/mnt/VBoxLinuxAdditions.run
+#umount /mnt
